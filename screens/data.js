ed0f5e2cc1db230f412adfe466ed5d81a419fad8
Charger1256A <aditya.ram112005@gmail.com>
2022-02-12 15:52:58 -0800

    test

--- screens/data.js
+++ screens/data.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { getFirestore, setDoc, doc, addDoc, collection } from "firebase/firestore";
+
 
 export default function Data() {
     return (
